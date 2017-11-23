1ee3f2a3e629b8e066691050c29434705762e977
opuerto <oscar.puertohn@gmail.com>
2017-11-23 12:27:04 +0000

    location drop down
    Todo : fix when the dropdown should hide.

--- js/geysir.js
+++ js/geysir.js
@@ -5,3 +5,19 @@ if (results == null){
 }
 return results[1] || 0;
 }
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!(event.target.matches('.dwn__select-default') || event.target.matches('.icon') || event.target.matches('.locTxt') || event.target.matches('.dwn__select-btn'))) {
+
+    var dropdowns = document.getElementsByClassName("dwn__select-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
