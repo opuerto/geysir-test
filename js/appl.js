861f466f022267837b65a1720dd9981e123b2a47
opuerto <oscar.puertohn@gmail.com>
2017-11-23 17:25:42 +0000

    price location on dropdown
    function that update the priceLocation

--- js/appl.js
+++ js/appl.js
@@ -9,28 +9,44 @@ const apiInfo = {
 
 Product.controller('mainCtrl', function($scope,
   $location, slidesSvc,
-  getVehicleByIdSvc, getVehicleByIdSvc,
-  getPriceForProduct
+  getVehicleByIdSvc, getPriceForProduct,
+  getProductLocations
 ) {
-  $scope.rentalPrice = 0;
-  var now = moment();
-  var timeSettings = {
+  var now, timeSettings, vehicleData,
+  pickUpLocationList, dropOffLocationList;
+  now = moment()
+  timeSettings = {
       startDate : now.format('DD/MM/YYYY'),
       endDate : now.format('DD/MM/YYYY'),
       startTime : "00:00:00",
       endTime : "00:00:00"
   }
   //call the service to fetch the data about the vehicle
-  var vehicleData = null;
+  vehicleData = null;
+  pickUpLocationList = [];
+  dropOffLocationList = [];
+  $scope.pickUpL = pickUpLocationList;
+  $scope.dropOff = dropOffLocationList;
+  $scope.rentalPrice = 0;
+  $scope.locationPrice = 0;
+
+
   getVehicleByIdSvc.fetchData().then(function(response) {
     vehicleData = response.data.result;
     //console.log(vehicleData);
   });
 
   //This section call the service to fetch the list of product locations
-  getVehicleByIdSvc.fetchData().then(function(response) {
-    var productLocation = response.data;
-    console.log(productLocation);
+  getProductLocations.fetchData().then(function(response) {
+    var productLocations = response.data.result;
+    pickUpLocationList = getProductLocationByType(0,productLocations);
+    dropOffLocationList = getProductLocationByType(1,productLocations);
+    $scope.pickUpL = pickUpLocationList;
+    $scope.dropOff = dropOffLocationList;
+    updateLocationPrice(pickUpLocationList,timeSettings.startTime,timeSettings.endTime);
+    updateLocationPrice(dropOffLocationList,timeSettings.startTime,timeSettings.endTime);
+
+
   });
 
 
@@ -81,6 +97,10 @@ Product.controller('mainCtrl', function($scope,
         //Check we should charche an extra day and show the message
         showDayExtraMessage(timeSettings.startTime,timeSettings.endTime);
 
+        // update the price of the locations
+        updateLocationPrice(pickUpLocationList,timeSettings.startTime,timeSettings.endTime);
+        updateLocationPrice(dropOffLocationList,timeSettings.startTime,timeSettings.endTime);
+
     });
  });
 
@@ -95,6 +115,10 @@ Product.controller('mainCtrl', function($scope,
        timeSettings.endTime = endTime;
        showDayExtraMessage(timeSettings.startTime,timeSettings.endTime);
 
+       // update the price of the locations
+       updateLocationPrice(pickUpLocationList,timeSettings.startTime,timeSettings.endTime);
+       updateLocationPrice(dropOffLocationList,timeSettings.startTime,timeSettings.endTime);
+
    });
 });
 
@@ -108,6 +132,61 @@ showDayExtraMessage = function(timeStart,timeEnd) {
       console.log($scope.showExtraDayMessage);
     }
 }
+
+getProductLocationByType = function(type,products) {
+  var list;
+  list = [];
+  for (var i = 0; i < products.length; i++) {
+    if(products[i].type === type && products[i].code !== "<ALL>") {
+      products[i].showPrice = 0;
+      list.push(products[i]);
+
+    }
+  }
+  return list;
+}
+
+updateLocationPrice = function (list,startTime,endTime) {
+    var workStart, workEnd, WsHours,WeHours,WsMinutes,WeMinutes;
+
+  for (var i = 0; i < list.length; i++) {
+      WsHours = list[i].workStart.split('PT').
+      pop().split('H').shift();
+      WeHours = list[i].workEnd.split('PT').
+      pop().split('H').shift();
+      WsMinutes = list[i].workStart.split('H').
+      pop().split('M').shift();
+      WeMinutes = list[i].workEnd.split('H').
+      pop().split('M').shift();
+      workStart = WsMinutes.length > 0 ? WsHours+":"
+      +WsMinutes :WsHours+":00";
+      workEnd = WeMinutes.length > 0 ? WeHours+":"
+      +WeMinutes :WeHours+":00";
+      if(moment(startTime,"HH:mm:ss").isBefore(moment(workStart,"HH:mm"))) {
+          // time selected for start time is before working hours
+          //set the showPrice to the extra price
+          list[i].showPrice = list[i].extraPrice;
+        } else if (moment(startTime,"HH:mm:ss").isAfter(moment(workEnd,"HH:mm"))) {
+          // time selected for start time is after working hours
+          //set the showPrice to the extra price
+          list[i].showPrice = list[i].extraPrice;
+        } else if (moment(endTime,"HH:mm:ss").isBefore(moment(workStart,"HH:mm"))) {
+          // time selected for end time if before working hours
+          //set the showPrice to the extra price
+          list[i].showPrice = list[i].extraPrice;
+        } else if (moment(endTime,"HH:mm:ss").isAfter(moment(workEnd,"HH:mm"))) {
+          // time selected for end time if after working hours
+          //set the showPrice to the extra price
+          list[i].showPrice = list[i].extraPrice;
+
+      } else {
+         // the selected time is between working hours
+         list[i].showPrice = list[i].price;
+      }
+  }
+}
+
+
 
 $scope.displayList = function (contentId) {
   /*when the user click on the select,
@@ -196,7 +275,7 @@ Product.service('getVehicleByIdSvc', function($http) {
 });
 
 //Service GetProductLocations
-Product.service('getVehicleByIdSvc', function($http) {
+Product.service('getProductLocations', function($http) {
   //product id variable
   var productId = $.urlParam('product');
   //set a default product to economy
